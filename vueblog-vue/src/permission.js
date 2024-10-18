679da69396e95d41a7b649f64d9b07387987f92d
sinxcosxtans <2961980835@qq.com>
2024-10-18 20:02:00 +0800

    学习permission.js

--- vueblog-vue/src/permission.js
+++ vueblog-vue/src/permission.js
@@ -1,9 +1,10 @@
 import router from "./router";
 
 // 路由判断登录 根据路由配置文件的参数
+//beforeEach 方法设置全局前置守卫，在每次路由变化前执行
 router.beforeEach((to, from, next) => {
 
-  if (to.matched.some(record => record.meta.requireAuth)) { // 判断该路由是否需要登录权限
+  if (to.matched.some(record => record.meta.requireAuth)) { // 通过判断该路由是否需要登录权限
 
     const token = localStorage.getItem("token")
     console.log("------------" + token)
@@ -12,14 +13,14 @@ router.beforeEach((to, from, next) => {
       if (to.path === '/login') {
 
       } else {
-        next()
+        next() // 继续访问目标路由
       }
     } else {
       next({
-        path: '/login'
+        path: '/login' // 如果没有token，重定向到登录页面
       })
     }
   } else {
-    next()
+    next() // 如果该路由不需要认证，继续访问
   }
 })
